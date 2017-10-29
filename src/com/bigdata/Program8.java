10676a46f556dfceb348313cb98f372a4f47edcb
MANJUNATH <MANJUNATH@SAMSUNG.Manjunath>
2017-10-29 22:41:37 +0530

    modified

--- src/com/bigdata/Program8.java
+++ src/com/bigdata/Program8.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 public class Program8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Integer a[] = new Integer[] { 1, 4, 6, 3, 2, 7, 8 };
 		Arrays.sort(a, Collections.reverseOrder());
 		System.out.println("Array in sorted descending order: \n");
