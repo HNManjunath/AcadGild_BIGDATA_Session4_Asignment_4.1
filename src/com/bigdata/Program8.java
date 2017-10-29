21916e4afebcc1a11995520edcef4aa6143b2c41
MANJUNATH <MANJUNATH@SAMSUNG.Manjunath>
2017-10-29 22:45:39 +0530

    added

--- src/com/bigdata/Program8.java
+++ src/com/bigdata/Program8.java
@@ -6,7 +6,9 @@ import java.util.Collections;
 public class Program8 {
 
 	public static void main(String[] args) {
+
 		Integer a[] = new Integer[] { 1, 4, 6, 3, 2, 7, 8 };
+		
 		Arrays.sort(a, Collections.reverseOrder());
 		System.out.println("Array in sorted descending order: \n");
 		for (int data : a) {
